89127f619c2ae97531059b422710fc4025a5c8c9
Mark Murray <m@rkmurray.co>
2018-03-15 20:31:55 +1100

    added url and view for the homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('blogs/', views.BlogListView.as_view(), name='bloglist'),
+    path('', views.index, name='homepage'),
+    path('blog/blogs', views.BlogListView.as_view(), name='bloglist'),
     path('blogger/<int:pk>', views.AuthorDetailView.as_view(), name='authorbloglist'),
 
 ]
